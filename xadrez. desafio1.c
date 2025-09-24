e3c34a76d7097aa22ea06bc2a240223409d5d6b3
henrychrisostomo1 <HENRYCHRISOSTOMO1@GMAIL.COM>
2025-09-24 13:13:04 +0000

    1° desafio de Xadrez

---
+++ xadrez. desafio1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+     
+    int i;
+
+    // mover a Torre 5 casas para a direita
+    for (int i = 0; i < 5; i++) {
+       printf("Direita \n"); // Imprime a direção do movimento
+    }
+    
+
+
+    return 0;
+}
