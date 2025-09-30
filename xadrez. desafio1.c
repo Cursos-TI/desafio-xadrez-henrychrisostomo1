6c289920154c64abdf94593bbe7d424458829374
henrychrisostomo1 <HENRYCHRISOSTOMO1@GMAIL.COM>
2025-09-30 12:14:07 +0000

    xadrez. desafio1.c

--- xadrez. desafio1.c
+++ xadrez. desafio1.c
@@ -2,14 +2,27 @@
 
 int main (){
      
-    int i;
+    int i = 1;
 
     // mover a Torre 5 casas para a direita
     for (int i = 0; i < 5; i++) {
        printf("Direita \n"); // Imprime a direção do movimento
     }
     
-
+    // mover o Bispo 5 casas para cima, direita (diagonal)
+    while (i <= 5)
+    {
+        printf("Cima,Direita \n"); // Imprime direção do movimento
+        i++;
+    }
+  
+    // mover o Rainha 8 casas para a esquerda
+        do
+        {
+            printf("Esquerda \n"); // Imprime direção do movimento
+            i++;
+        } while (i <= 8);
+        
 
     return 0;
 }
