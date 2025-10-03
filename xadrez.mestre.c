9455469c083b314fba1586d61568feb110cf10e3
henrychrisostomo1 <HENRYCHRISOSTOMO1@GMAIL.COM>
2025-10-03 16:37:46 +0000

    xadrez.mestre.c

--- xadrez.mestre.c
+++ xadrez.mestre.c
@@ -5,23 +5,39 @@ void moverTorre (int casas){
         printf("Direita \n");
         moverTorre(casas - 1);
         
-
+      }
      }
-}
-
-int main (){ 
-    moverTorre(5);
-
-    void moverBispo (int casas){
+     void moverBispo (int casas){
      if (casas > 0){
         printf("Cima, Direita \n");
         moverBispo(casas - 1);
         
-
+      
      }
-}
+    } 
+    void moverRainha (int casas){
+     if (casas > 0){
+        printf("Esquerda \n");
+        moverRainha(casas - 1);
+        
+      }
+     }
 
 int main (){ 
+    moverTorre(5);
     moverBispo(5);
+    moverRainha(8);
+    int i = 1, movimentoCompleto = 1;
+    
+    while (movimentoCompleto--)
+         {
+            for (int i = 0; i < 2; i++) {
+                printf("Cima \n"); // Imprime "Cima" duas vezes
+            }
+            printf("Direita \n"); //Imprime "Direita" uma vez
+         }
     return 0;
-}
+ }
+
+
+    
